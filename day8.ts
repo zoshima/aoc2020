42427e6da58c86a9899f88dddd4d45f996213868
Dmytro Krasovsky <dmykras@gmail.com>
2021-02-28 20:32:14 +0100

    day 9 pt 2

--- day8.ts
+++ day8.ts
@@ -31,12 +31,57 @@ function getAddendants(
   return null;
 }
 
+function getContiguousSet(
+  num: number
+): { set: number[]; min: number; max: number } | null {
+  const lim = input.length / 2;
+
+  let setLength = 2;
+
+  do {
+    for (let i = 0; i < input.length - setLength; i++) {
+      let sum = 0;
+      let min = input[i];
+      let max = min;
+
+      for (let j = 0; j < setLength; j++) {
+        const _num = input[i + j];
+
+        sum += _num;
+
+        if (_num < min) {
+          min = _num;
+        }
+
+        if (_num > max) {
+          max = _num;
+        }
+      }
+
+      if (sum === num) {
+        return {
+          set: input.slice(i, i + setLength),
+          min: min,
+          max: max,
+        };
+      }
+    }
+  } while (++setLength < lim);
+
+  return null;
+}
+
 for (let i = preample; i < input.length; i++) {
   const num = input[i];
 
   if (!getAddendants(num, i)) {
-    console.log(num);
-    Deno.exit();
+    // 127
+    const set = getContiguousSet(num);
+
+    if (set) {
+      console.log(set, set.min + set.max);
+      Deno.exit();
+    }
   }
 }
 
