7746efae60d8654eb846cdcc956df9181d98277b
Dmytro Krasovsky <dmykras@gmail.com>
2021-06-02 01:10:16 +0200

    day 14 pt 2

--- src/day14.cpp
+++ src/day14.cpp
@@ -1,10 +1,10 @@
+#include <bitset>
 #include <fstream>
 #include <iostream>
-#include <string>
-#include <vector>
 #include <regex>
+#include <string>
 #include <unordered_map>
-#include <bitset>
+#include <vector>
 
 using namespace std;
 
@@ -13,32 +13,47 @@ const regex memPattern("^mem\\[(\\d+)\\] = (\\d+)$");
 
 const int kMaskSize = 36;
 
+void ApplyMask(unordered_map<unsigned long, int> &addresses, const int &value,
+               const string &mask, bitset<kMaskSize> bset, int i = 0) {
+  for (; i < kMaskSize; i++) {
+    switch (mask[i]) {
+    case '0':
+      break;
+    case '1':
+      bset.set(i, 1);
+      break;
+    case 'X':
+      bset.set(i, 1);
+      ApplyMask(addresses, value, mask, bset, i + 1);
+      bset.set(i, 0);
+      ApplyMask(addresses, value, mask, bset, i + 1);
+      return;
+    }
+  }
+
+  addresses[bset.to_ulong()] = value;
+}
+
 int main() {
   ifstream file("../input/day14.txt");
   string line, mask;
   smatch sm;
-  unordered_map<int, unsigned long> memory;
+  unordered_map<unsigned long, int> memory;
 
   while (getline(file, line)) {
     if (line[1] == 'a') {
       regex_search(line, sm, maskPattern);
 
       mask = sm[1];
+
+      reverse(mask.begin(), mask.end());
     } else {
       regex_search(line, sm, memPattern);
 
       int index = stoi(sm[1]);
-      bitset<kMaskSize> value(stoi(sm[2]));
+      int value = stoi(sm[2]);
 
-      for (int i = 0; i < kMaskSize; i++) {
-        if (mask[i] == '1') {
-          value.set(kMaskSize - i - 1, 1);
-        } else if (mask[i] == '0') {
-          value.set(kMaskSize - i - 1, 0);
-        }
-      }
-
-      memory[index] = value.to_ulong();
+      ApplyMask(memory, value, mask, bitset<kMaskSize>(index));
     }
   }
 
