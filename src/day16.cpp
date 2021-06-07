eac8d37360b1fa8374046b7d81dd3fdfe58db5f3
Dmytro Krasovsky <dmykras@gmail.com>
2021-06-07 20:13:23 +0200

    day 16 pt 2

--- src/day16.cpp
+++ src/day16.cpp
@@ -1,9 +1,10 @@
 #include <fstream>
 #include <iostream>
-#include <numeric>
 #include <regex>
 #include <sstream>
+#include <unordered_map>
 #include <vector>
+#include <array>
 
 using namespace std;
 
@@ -12,14 +13,17 @@ struct Range {
   int end;
 };
 
+const regex kRangePattern("^(.+):\\ (\\d+)-(\\d+) or (\\d+)-(\\d+)");
+const int kNumNumbers = 20;
+
 using Ticket = vector<int>;
 using RangePair = pair<Range, Range>;
-
-const regex range_pattern("^.+:\\ (\\d+)-(\\d+) or (\\d+)-(\\d+)");
+using NumbersArray = array<int, kNumNumbers>;
 
 Ticket my_ticket;
 vector<Ticket> nearby_tickets;
 vector<RangePair> ranges;
+vector<string> range_names;
 
 Ticket GetTicket(const string &s) {
   istringstream ss(s);
@@ -39,9 +43,10 @@ void LoadInput() {
   smatch sm;
 
   while (getline(file, line) && line.size()) {
-    regex_search(line, sm, range_pattern);
+    regex_search(line, sm, kRangePattern);
 
-    ranges.push_back({{stoi(sm[1]), stoi(sm[2])}, {stoi(sm[3]), stoi(sm[4])}});
+    range_names.push_back(sm[1]);
+    ranges.push_back({{stoi(sm[2]), stoi(sm[3])}, {stoi(sm[4]), stoi(sm[5])}});
   }
 
   getline(file, line); // "your ticket:"
@@ -57,7 +62,7 @@ void LoadInput() {
   }
 }
 
-const bool Includes(const Range &range, const int num) {
+inline const bool Includes(const Range &range, const int &num) {
   for (int i = range.start; i <= range.end; ++i) {
     if (i == num) {
       return true;
@@ -67,27 +72,107 @@ const bool Includes(const Range &range, const int num) {
   return false;
 }
 
-int main() {
-  LoadInput();
+const vector<int> GetRangeIndices(const int &num) {
+  vector<int> indices;
 
-  vector<int> invalid_values;
+  for (int i = 0; i < ranges.size(); ++i) {
+    if (Includes(ranges[i].first, num) || Includes(ranges[i].second, num)) {
+      indices.push_back(i);
+    }
+  }
 
-  for (const auto &ticket : nearby_tickets) {
-    for (const int &i : ticket) {
-      bool is_valid = false;
+  return indices;
+}
 
-      for (const RangePair &rp : ranges) {
-        if (Includes(rp.first, i) || Includes(rp.second, i)) {
-          is_valid = true;
-          break;
-        }
+const unordered_map<int, vector<vector<int>>> GetValidTickets() {
+  unordered_map<int, vector<vector<int>>> valid_tickets;
+
+  for (int i = 0; i < nearby_tickets.size(); ++i) {
+    vector<vector<int>> property_indices_map;
+    bool is_valid = true;
+
+    for (const auto &ticket_property : nearby_tickets[i]) {
+      vector<int> range_indices = GetRangeIndices(ticket_property);
+
+      if (range_indices.size() == 0) {
+        // invalid ticket
+        is_valid = false;
+        break;
       }
 
-      if (!is_valid) {
-        invalid_values.push_back(i);
+      property_indices_map.push_back(range_indices);
+    }
+
+    if (is_valid) {
+      valid_tickets.insert({i, property_indices_map});
+    }
+  }
+
+  return valid_tickets;
+}
+
+void PopulateIndices(NumbersArray &ordered_indices,
+               const array<NumbersArray, kNumNumbers> &range_count_map,
+               const int &num_elements, const int target = 1) {
+  if (target > kNumNumbers) {
+    return;
+  }
+
+  for (int i = 0; i < kNumNumbers; ++i) {
+    auto range_counts = range_count_map[i];
+    auto c = count(range_counts.begin(), range_counts.end(), num_elements);
+
+    if (c == target) {
+      for (int j = 0; j < kNumNumbers; ++j) {
+        if (range_counts[j] == num_elements) {
+          auto it = find(ordered_indices.begin(), ordered_indices.end(), j);
+
+          if (it == ordered_indices.end()) {
+            ordered_indices[i] = j;
+            break;
+          } 
+        }
       }
     }
   }
 
-  cout << accumulate(invalid_values.begin(), invalid_values.end(), 0) << endl;
+  PopulateIndices(ordered_indices, range_count_map, num_elements, target + 1);
+}
+
+int main() {
+  LoadInput();
+
+  const auto valid_tickets = GetValidTickets();
+  const int num_valid_tickets = valid_tickets.size();
+
+  NumbersArray ordered_indices;
+  ordered_indices.fill(-1);
+
+  array<NumbersArray, kNumNumbers> range_count_map; // index: property pos
+
+  for (int i = 0; i < kNumNumbers; ++i) {
+    NumbersArray range_counts;
+    range_counts.fill(0);
+
+    // populate map
+    for (const auto &ticket : valid_tickets) {
+      for (const int &range_index : ticket.second[i]) {
+        range_counts[range_index] += 1;
+      }
+    }
+
+    range_count_map[i] = range_counts;
+  }
+
+  PopulateIndices(ordered_indices, range_count_map, num_valid_tickets);
+
+  long result = 1;
+
+  for (int i = 0; i < kNumNumbers; ++i) {
+    if (range_names[ordered_indices[i]].rfind("departure", 0) == 0) {
+      result *= my_ticket[i];
+    }
+  }
+
+  cout << result << endl;
 }
